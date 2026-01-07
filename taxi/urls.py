565788fdbff8faef93ac8d8cd21ce9c13b3cb157
GavrilenkoVladimir <vladimirvgavrilenko@gmail.com>
2026-01-07 14:24:48 +0200

    Finished task with style

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,9 +1,25 @@
 from django.urls import path
 
-from .views import index
+from .views import (
+    index,
+    DriversView,
+    DriversDetailView,
+    CarsView,
+    CarsDetailView,
+    ManufacturersView
+)
 
 urlpatterns = [
     path("", index, name="index"),
+    path("drivers/", DriversView.as_view(), name="drivers"),
+    path(
+        "drivers/<int:pk>",
+        DriversDetailView.as_view(),
+        name="drivers_detail"
+    ),
+    path("cars/", CarsView.as_view(), name="cars"),
+    path("cars/<int:pk>", CarsDetailView.as_view(), name="cars_detail"),
+    path("manufacturers/", ManufacturersView.as_view(), name="manufacturers"),
 ]
 
 app_name = "taxi"
--- taxi/views.py
+++ taxi/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
 from taxi.models import Driver, Car, Manufacturer
 
@@ -13,3 +14,26 @@ def index(request):
     }
 
     return render(request, "taxi/index.html", context=context)
+
+
+class DriversView(generic.ListView):
+    model = Driver
+    paginate_by = 5
+
+
+class DriversDetailView(generic.DetailView):
+    model = Driver
+
+
+class CarsView(generic.ListView):
+    model = Car
+    paginate_by = 5
+
+
+class CarsDetailView(generic.DetailView):
+    model = Car
+
+
+class ManufacturersView(generic.ListView):
+    model = Manufacturer
+    paginate_by = 5
